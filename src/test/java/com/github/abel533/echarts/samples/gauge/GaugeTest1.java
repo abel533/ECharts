4462b02c57fcfbcce67609fa57c0fa66bf5a645c
abel533 <abel533@gmail.com>
2014-09-17 12:35:52 +0800

    新增大规模散点图例子。

--- src/test/java/com/github/abel533/echarts/samples/gauge/GaugeTest1.java
+++ src/test/java/com/github/abel533/echarts/samples/gauge/GaugeTest1.java
@@ -38,10 +38,12 @@ public class GaugeTest1 {
 
     @Test
     public void test() {
+        // 地址： http://echarts.baidu.com/doc/example/gauge1.html
         EnhancedOption option = new EnhancedOption();
         option.tooltip().formatter("{a} <br/>{b} : {c}%");
         option.toolbox().show(true).feature(Tool.mark, Tool.restore, Tool.saveAsImage);
         option.series(new Gauge("业务指标").detail(new Detail().formatter("{value}%")).data(new Data("完成率", 75)));
+        option.exportToHtml("guage1.html");
         option.view();
     }
 }
