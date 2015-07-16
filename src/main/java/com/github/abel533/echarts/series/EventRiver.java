d07cf893255a8396a08fab1c7c0bd4e959bfbd91
isea533 <abel533@gmail.com>
2015-07-16 14:24:07 +0800

    2.2.1- [eventRiver] eventList 配置项名统一为 data。

--- src/main/java/com/github/abel533/echarts/series/EventRiver.java
+++ src/main/java/com/github/abel533/echarts/series/EventRiver.java
@@ -25,11 +25,6 @@
 package com.github.abel533.echarts.series;
 
 import com.github.abel533.echarts.code.SeriesType;
-import com.github.abel533.echarts.series.event.Event;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * 事件河流图
@@ -40,7 +35,6 @@ public class EventRiver extends Series<EventRiver> {
      * 该事件类别的权重
      */
     private Integer weight;
-    private List<Event> eventList;
 
     /**
      * 构造函数
@@ -89,40 +83,6 @@ public class EventRiver extends Series<EventRiver> {
     }
 
     /**
-     * 获取eventList值
-     */
-    public List<Event> eventList() {
-        if (this.eventList == null) {
-            this.eventList = new ArrayList<Event>();
-        }
-        return this.eventList;
-    }
-
-    /**
-     * 设置eventList值
-     *
-     * @param eventList
-     */
-    public EventRiver eventList(List<Event> eventList) {
-        this.eventList = eventList;
-        return this;
-    }
-
-    /**
-     * 添加eventList值
-     *
-     * @param values
-     * @return
-     */
-    public EventRiver eventList(Event... values) {
-        if (values == null || values.length == 0) {
-            return this;
-        }
-        this.eventList().addAll(Arrays.asList(values));
-        return this;
-    }
-
-    /**
      * 获取weight值
      */
     public Integer getWeight() {
@@ -136,21 +96,5 @@ public class EventRiver extends Series<EventRiver> {
      */
     public void setWeight(Integer weight) {
         this.weight = weight;
-    }
-
-    /**
-     * 获取eventList值
-     */
-    public List<Event> getEventList() {
-        return eventList;
-    }
-
-    /**
-     * 设置eventList值
-     *
-     * @param eventList
-     */
-    public void setEventList(List<Event> eventList) {
-        this.eventList = eventList;
     }
 }
