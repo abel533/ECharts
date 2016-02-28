d65e5d6e3c7da38b1ae5256576468b64f37f838e
isea533 <abel533@gmail.com>
2016-02-28 09:56:22 +0800

    首字母都改为小写

--- src/main/java/com/github/abel533/echarts/code/Easing.java
+++ src/main/java/com/github/abel533/echarts/code/Easing.java
@@ -31,15 +31,15 @@ package com.github.abel533.echarts.code;
  * @since 2015-07-16
  */
 public enum Easing {
-    Linear,
-    QuadraticIn, QuadraticOut, QuadraticInOut,
-    CubicIn, CubicOut, CubicInOut,
-    QuarticIn, QuarticOut, QuarticInOut,
-    QuinticIn, QuinticOut, QuinticInOut,
-    SinusoidalIn, SinusoidalOut, SinusoidalInOut,
-    ExponentialIn, ExponentialOut, ExponentialInOut,
-    CircularIn, CircularOut, CircularInOut,
-    ElasticIn, ElasticOut, ElasticInOut,
-    BackIn, BackOut, BackInOut,
-    BounceIn, BounceOut, BounceInOut
+    linear,
+    quadraticIn, quadraticOut, quadraticInOut,
+    cubicIn, cubicOut, cubicInOut,
+    quarticIn, quarticOut, quarticInOut,
+    quinticIn, quinticOut, quinticInOut,
+    sinusoidalIn, sinusoidalOut, sinusoidalInOut,
+    exponentialIn, exponentialOut, exponentialInOut,
+    circularIn, circularOut, circularInOut,
+    elasticIn, elasticOut, elasticInOut,
+    backIn, backOut, backInOut,
+    bounceIn, bounceOut, bounceInOut
 }
