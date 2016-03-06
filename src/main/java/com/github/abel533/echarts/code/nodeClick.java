68ea3766ff8fca3ab16aed35ffb173cccb8febe4
isea533 <abel533@gmail.com>
2016-03-06 11:02:49 +0800

    3.0.0-SNAPSHOT

---
+++ src/main/java/com/github/abel533/echarts/code/nodeClick.java
@@ -0,0 +1,12 @@
+package com.github.abel533.echarts.code;
+
+/**
+ * 点击节点
+ *
+ * @author liuzh
+ * @since 2016-02-28 10:50
+ */
+public enum NodeClick {
+    zoomToNode, //点击节点后缩放到节点。
+    link,       //如果节点数据中有 link 点击节点后会进行超链接跳转。
+}
